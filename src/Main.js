c461e2471265a1a90a8e229f38546d05e9757c25
Divine Mbunga <divinembunga7@gmail.com>
2019-11-20 11:49:50 +0000

    can display some data, database needs to be implemeted

--- src/Main.js
+++ src/Main.js
@@ -1,21 +1,40 @@
 import React, { Component } from "react";
 
+
+fetch('https://api.github.com/users/divinembunga/followers')
+    .then(response => {
+        return response.json()
+    })
+    .then(data => {
+        // Work with JSON data here
+        // var myObj = JSON.parse(this.data);
+        // document.getElementById("demo").innerHTML = myObj[1];
+        console.log(data)
+        const{login, followers_url} = data[1];
+        document.getElementById('l').textContent = login;
+        document.getElementById('f').textContent = followers_url;
+        //user_profile.social_media[1].description
+    })
+    .catch(err => {
+        // Do something for an error here
+    })
 class Main extends Component {
+
     render() {
         return (
-            fetch('https://api.github.com/users/divinembunga/followers')
-                .then(response => {
-                    return response.json()
-                })
-                .then(data => {
-                    // Work with JSON data here
-                    console.log(data)
-                    return data
+            <div>
+                <h1>Social Graph</h1>
+                <p>
+                    login: <span id ="l"></span>
+                </p>
+                <p>
+                    follower: <span id ="f"></span>
+                </p>
 
-                })
-                .catch(err => {
-                    // Do something for an error here
-                })
+                <div className="content">
+
+                </div>
+            </div>
         );
     }
 }
--- src/scripts.js
+++ src/scripts.js
@@ -1,13 +1,18 @@
 
 // Replace ./data.json with your JSON feed
+import {Argv as json} from "yargs";
+
 fetch('https://api.github.com/users/divinembunga/followers')
     .then(response => {
         return response.json()
     })
     .then(data => {
         // Work with JSON data here
+       // var myObj = JSON.parse(this.data);
+       // document.getElementById("demo").innerHTML = myObj[1];
         console.log(data)
-        alert(data[0].login);
+        const{login, followers_url} = data[0];
+
         //user_profile.social_media[1].description
     })
     .catch(err => {
